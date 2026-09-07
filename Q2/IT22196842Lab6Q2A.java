043f108d680a8b40460fa6aa7b9861690de6bd15
it22196842 <it22196842@my.sliit.lk>
2026-09-07 18:25:04 +0530

    Add IT22196842Lab6Q2.java to Q2A

---
+++ Q2/IT22196842Lab6Q2A.java
@@ -0,0 +1,12 @@
+public class IT22196842Lab6Q2A {
+    public static void main(String[] args) {
+	
+	 int number = 1;
+	
+	 while (number <= 10) {
+	      System.out.print(number + " " );
+		  number++;
+	 }
+	
+	}
+}
