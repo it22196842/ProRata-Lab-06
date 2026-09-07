7f153f13073eba3a3be988f0aaa980d85323a428
it22196842 <it22196842@my.sliit.lk>
2026-09-07 18:25:51 +0530

    Add IT22196842Lab6Q2.java to Q2B

---
+++ Q2/IT22196842Lab6Q2B.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class IT22196842Lab6Q2B {
+    public static void main(String[] args) {
+	
+	  Scanner input = new Scanner(System.in);
+	
+	  int Numbers;
+	
+	  System.out.println("Please enter 10 numbers: ");
+	
+	  System.out.print("Enter number 1: ");
+	  int number1 = input.nextInt();
+	  
+	  System.out.print("Enter number 2: ");
+	  int number2 = input.nextInt();
+	  
+	  System.out.print("Enter number 3: ");
+	  int number3 = input.nextInt();
+	  
+	  System.out.print("Enter number 4: ");
+	  int number4 = input.nextInt();
+	  
+	  System.out.print("Enter number 5: ");
+	  int number5 = input.nextInt();
+	  
+	  System.out.print("Enter number 6: ");
+	  int number6 = input.nextInt();
+	  
+	  System.out.print("Enter number 7: ");
+	  int number7 = input.nextInt();
+	  
+	  System.out.print("Enter number 8: ");
+	  int number8 = input.nextInt();
+	  
+	  System.out.print("Enter number 9: ");
+	  int number9 = input.nextInt();
+	  
+	  System.out.print("Enter number 10: ");
+	  int number10 = input.nextInt();
+	  
+	  System.out.println("The numbers you entered are: ");
+	  System.out.println();
+	  System.out.println( +number1 + " " +number2 + " " +number3 + " " +number4 + " " +number5 + " " +number6 + " " +number7 + " " +number8 + " " +number9 + " " +number10 );
+	  
+	}
+}
+	  
+	   
